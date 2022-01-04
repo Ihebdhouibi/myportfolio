287abfe17e24212eb8eed661e00a6a7011412ed3
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-01-04 01:42:05 +0100

    mobile devices responsive styles

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// toggle btn
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-container');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linkContainer.classList.toggle('show');
+});
+
+
 (function ($){
     "use strict"
 
